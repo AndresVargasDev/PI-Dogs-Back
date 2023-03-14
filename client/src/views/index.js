979e6928b6b5446dddae9222ff3f7aafe3a25278
akanashimi <andresvargas094@gmail.com>
2023-03-14 10:39:34 -0600

    Separacion de codigo de back en nuevo repo

--- client/src/views/index.js
+++
@@ -1,8 +0,0 @@
-import LandingPage from "./LandingPage/LandingPage";
-import Home from "./Home/Home";
-import Create from "./Create/Create";
-import DetailDog from './DetailDog/DetailDog';
-import Error from './Error/Error'
-import Modify from './Modify/Modify'
-
-export { LandingPage, Home, Create, DetailDog, Error, Modify }
